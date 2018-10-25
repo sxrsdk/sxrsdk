7700aefe2c838f09915b889436af19227e86b45d
Nola Donato <nola.donato@samsung.com>
2018-10-25 15:00:35 -0700

    docs

--- GVRf/Framework/framework/src/main/java/org/gearvrf/animation/GVRBodyTracker.java
+++ GVRf/Framework/framework/src/main/java/org/gearvrf/animation/GVRBodyTracker.java
@@ -17,6 +17,33 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
+/**
+ * Handles gathering camera images for a body tracker.
+ * <p>
+ * A body tracker inspects a series of camera images
+ * and updates the current pose of a target skeleton
+ * based on the input images.
+ * <p>
+ * This class provides a framework for body tracking.
+ * It does not actually implement tracking of any kind.
+ * It exists as an interface to body tracker subclasses.
+ * It is responsible for opening the camera and
+ * gather camera images for body tracking.
+ * <p>
+ * A body tracker is also a {@link GVREventReceiver} and
+ * can emit {@TrackerEvents} to its listeners indicating
+ * when tracking starts, stops and when an image produces
+ * tracking data.
+ * <p>
+ * Subclasses must provide implementations for several
+ * functions:
+ * <ul>
+ * <li>trackFromImage - produces body tracking data from a camera image</li>
+ * <li>updateSkeleton - updates the target skeleton from body tracking data</li>
+ * <li>isRunning      - true if body tracker is capturing image data</li>
+ * <li>isTracking     - true if body tracker is updating the skeleton</li>
+ * </ul>
+ */
 public abstract class GVRBodyTracker extends GVRComponent implements IEventReceiver
 {
     public interface TrackerEvents extends IEvents
@@ -36,6 +63,12 @@ public abstract class GVRBodyTracker extends GVRComponent implements IEventRecei
     private boolean mTryOpenCamera = true;
     protected GVREventReceiver mListeners;
 
+    /**
+     * Constructs a body tracker which updates a given skeleton.
+     * This constructor is intended for subclasses only.
+     * @param skel          target {@GVRSkeleton} to update
+     * @param nativePtr     native pointer to C++ implementation for body tracking
+     */
     protected GVRBodyTracker(GVRSkeleton skel, long nativePtr)
     {
         super(skel.getGVRContext(), nativePtr);
@@ -45,6 +78,12 @@ public abstract class GVRBodyTracker extends GVRComponent implements IEventRecei
         mListeners = new GVREventReceiver(this);
     }
 
+    /**
+     * Constructs a body tracker without a skeleton.
+     * This constructor is intended for subclasses only.
+     * @param context       {@GVRContext} which owns this tracker
+     * @param nativePtr     native pointer to C++ implementation for body tracking
+     */
     protected GVRBodyTracker(GVRContext context, long nativePtr)
     {
         super(context, nativePtr);
@@ -56,18 +95,44 @@ public abstract class GVRBodyTracker extends GVRComponent implements IEventRecei
 
     static public long getComponentType() { return NativeBodyTracker.getComponentType(); }
 
+    /**
+     * Get the {@GVREventReceiver} interface to add or remove
+     * listeners for {@TrackerEvents}
+     * @return {@GVREventReceiver} which generates tracking events.
+     */
     public GVREventReceiver getEventReceiver() { return mListeners; }
 
+    /**
+     * Get the {@GVRSkeleton} being updated by this body tracker
+     * @return skeleton or null if none
+     */
     public GVRSkeleton getSkeleton()
     {
         return mTargetSkeleton;
     }
 
+    /**
+     * Get the {@GVRCameraSceneObject} that owns the camera
+     * being used for tracking.
+     * <p>
+     * Adding this object to your scene will produce a display
+     * of the images being tracked.
+     * @return scene object to display camera
+     */
     public GVRCameraSceneObject getCameraDisplay()
     {
         return mCameraOwner;
     }
 
+    /**
+     * Start body tracking from camera.
+     * <p>
+     * This function opens the camera and begins capturing camera previews.
+     * It will throw exceptions if the camera cannot be accessed.
+     * @throws CameraAccessException
+     * @throws IOException
+     * @throws IllegalAccessException
+     */
     public void start() throws CameraAccessException, IOException, IllegalAccessException
     {
         if (mTryOpenCamera)
@@ -107,6 +172,11 @@ public abstract class GVRBodyTracker extends GVRComponent implements IEventRecei
         }
     }
 
+    /**
+     * Stop body tracking.
+     * <p>
+     * Stop capturing images and tracking from them.
+     */
     public void stop()
     {
         if (mCamera != null)
@@ -119,13 +189,38 @@ public abstract class GVRBodyTracker extends GVRComponent implements IEventRecei
         }
     }
 
+    /**
+     * True if the body tracker is capturing images
+     * @return true if camera preview images are being captured
+     */
     public abstract boolean isRunning();
 
+    /**
+     * True if the body tracker is tracking from images
+     * <p>
+     * The body tracker may run a separate thread which automatically
+     * captures images and tracks from them or it may rely on
+     * the application to call {#track()} every time it wants
+     * to track from an image.
+     * @return true if tracking thread is running, false if not
+     */
+    public abstract boolean isTracking();
+
+    /**
+     * Returns the most recently capture camera image
+     * @return camera image data, null if camera not previewing
+     */
     protected synchronized byte[] getImageData()
     {
         return mImageData;
     }
 
+    /**
+     * Called when the camera gets another image.
+     * It keeps a pointer to the captured image
+     * for the tracking thread.
+     * @param imageData new camera image data
+     */
     protected synchronized void  setImageData(byte[] imageData)
     {
         mImageData = imageData;
@@ -154,21 +249,68 @@ public abstract class GVRBodyTracker extends GVRComponent implements IEventRecei
         return false;
     }
 
+    /**
+     * Subclasses must implement this to produce body tracking
+     * data from the capture image.
+     * <p>
+     * The format and location of this data is the responsibility
+     * of the implementation and is not exposed by the framework.
+     * @param imageData most recently capture image
+     * @return true if tracking was successful, false on error
+     */
     abstract protected boolean trackFromImage(byte[] imageData);
 
-    abstract protected  boolean updateSkeleton();
+    /**
+     * Subclasses must implement this to update the target
+     * skeleton from the body tracking results.
+     * <p>
+     * This function will never be called if a target skeleton
+     * is not provided.
+     * @return true if skeleton was successfully updated, false on error
+     */
+    abstract protected boolean updateSkeleton();
 
+    /**
+     * Called when image capture starts.
+     * <p>
+     * This function emits the <b>onTrackStart</b> event.
+     * Subclasses which override it must call the parent
+     * implementation or events will not be correctly
+     * emitted. It is provided to allow implementation
+     * specific code when tracking begins.
+     * @return true if tracking can be started, false on error
+     */
     protected boolean onStart()
     {
         getGVRContext().getEventManager().sendEvent(this, TrackerEvents.class, "onTrackStart", this);
         return true;
     }
 
+    /**
+     * Called when image capture stops.
+     * <p>
+     * This function emits the <b>onTrackEnd</b> event.
+     * Subclasses which override it must call the parent
+     * implementation or events will not be correctly
+     * emitted. It is provided to allow implementation
+     * specific code when tracking ends.
+     */
     protected void onStop()
     {
         getGVRContext().getEventManager().sendEvent(this, TrackerEvents.class, "onTrackEnd", this);
     }
 
+    /**
+     * Opens the camera and starts capturing images.
+     * <p>
+     * The default implementation uses <i>ImageFormat.NV21</i>
+     * and tracks at 30fps. It captures 1280 x 960 images.
+     * The width and height is obtained from mWidth and mHeight
+     * so constructors may set this to change the image size.
+     * @return
+     * @throws CameraAccessException
+     * @throws IOException
+     */
     protected Camera openCamera() throws CameraAccessException, IOException
     {
         Camera camera = Camera.open();
